653035da53d5f302d4883407fa07de2edfe646a4
nguyentrungkien2180606198 <kienadu123@gmail.com>
2025-01-07 07:21:51 +0700

    van tay admin

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hutech_event_flutter/screens/admin/student/students_screen.dart';
+import 'package:local_auth/local_auth.dart';
+import '../../decistions_tree.dart';
 import 'event/events_screen.dart';
+import 'package:hutech_event_flutter/screens/admin/event/setting_screen.dart';
 
 class AdminDashboard extends StatefulWidget {
   const AdminDashboard({Key? key}) : super(key: key);
@@ -11,16 +15,57 @@ class AdminDashboard extends StatefulWidget {
 }
 
 class _AdminDashboardState extends State<AdminDashboard> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final LocalAuthentication auth = LocalAuthentication();
   int currentPageIndex = 0;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  String error = '';
 
   logout() async {
     await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    setState(() {
+      error = '';
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkuser();
+  }
+
+  Future<void> checkuser() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      bool authenticated = await authenticate();
+      if (!authenticated) {
+        logout();
+        Navigator.pushNamed(context, '/');
+      }
+    }
+  }
+
+  Future<bool> authenticate() async {
+    try {
+      bool authenticated = await auth.authenticate(
+        localizedReason: 'Please authenticate to access the admin dashboard',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
+      );
+      return authenticated;
+    } catch (e) {
+      return false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.deepPurple[50],
         title: const Text('Admin Dashboard'),
         actions: [
@@ -41,6 +86,7 @@ class _AdminDashboardState extends State<AdminDashboard> {
           StudentsScreen(),
           Text('Reports Screen'),
           Text('Notifications Screen'),
+          SettingScreen(),
         ],
       ),
       bottomNavigationBar: NavigationBar(
@@ -67,6 +113,7 @@ class _AdminDashboardState extends State<AdminDashboard> {
           NavigationDestination(icon: Icon(Icons.report), label: 'Reports'),
           NavigationDestination(
               icon: Icon(Icons.notifications), label: 'Notifications'),
+          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
         ],
       ),
     );
